bdcebf4b26a4c43fafda689ac04527879c7223d5
Alexandra Lomotan <alomotan@splunk.com>
2020-02-14 15:59:43 -0800

    PAPP-9145 Zscaler 2to3

--- zscaler_consts.py
+++ zscaler_consts.py
@@ -1,8 +1,9 @@
 # File: zscaler_consts.py
-# Copyright (c) 2017-2019 Splunk Inc.
+# Copyright (c) 2017-2020 Splunk Inc.
 #
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
+
 ZSCALER_JSON_FULL_DETAILS = 'Full Details'
 ZSCLAER_ERR_MD5_UNKNOWN_MSG = 'md5 is unknown or analysis has yet not been completed'
 ZSCALER_SANDBOX_GET_REPORT_MSG = 'Sandbox report successfully fetched for the provided md5 hash'
