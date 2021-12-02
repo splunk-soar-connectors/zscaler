cd2df0ef37e8cb88e02829bbeb45fc118a69b696
splunk-soar-connectors-admin <admin@splunksoar>
2021-12-02 09:16:19 -0800

    Updating source files with legal boilerplate

--- zscaler_consts.py
+++ zscaler_consts.py
@@ -1,9 +1,17 @@
 # File: zscaler_consts.py
+#
 # Copyright (c) 2017-2021 Splunk Inc.
 #
-# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
-# without a valid written license from Splunk Inc. is PROHIBITED.
-
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
 ZSCALER_JSON_FULL_DETAILS = 'Full Details'
 ZSCLAER_ERR_MD5_UNKNOWN_MSG = 'md5 is unknown or analysis has yet not been completed'
 ZSCALER_SANDBOX_GET_REPORT_MSG = 'Sandbox report successfully fetched for the provided md5 hash'
