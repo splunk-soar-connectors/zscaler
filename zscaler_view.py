547fe5539b79b27ec2a938fd6fc37fa059028360
achandya <achandya@splunk.com>
2022-01-21 09:43:47 +0530

    Remove additional brackets

--- zscaler_view.py
+++ zscaler_view.py
@@ -21,10 +21,10 @@ def get_ctx_result(result):
 
     ctx_result['param'] = param
 
-    if (data):
+    if data:
         ctx_result['data'] = data[0]
 
-    if (summary):
+    if summary:
         ctx_result['summary'] = summary
 
     return ctx_result
@@ -37,7 +37,7 @@ def display_view(provides, all_app_runs, context):
         for result in action_results:
 
             ctx_result = get_ctx_result(result)
-            if (not ctx_result):
+            if not ctx_result:
                 continue
             results.append(ctx_result)
 
